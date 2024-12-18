28eb714428e6e4c60c7aa9d62dc8d90130fd3ac5
ducdm1508 <duc.dm.2442@aptechlearning.edu.vn>
2024-12-19 02:37:12 +0700

    update

--- comics/src/Components/Header.jsx
+++ comics/src/Components/Header.jsx
@@ -1,52 +1,175 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import '../Styles/header.scss';
 
 const Header = () => {
-    // State để lưu trạng thái hiển thị dropdown
-    const [showDropdown, setShowDropdown] = useState(false);
-    const [selectedValue, setSelectedValue] = useState('Danh sách'); // Giá trị đã chọn mặc định
+    const [showDropdownDanhSach, setShowDropdownDanhSach] = useState(false);
+    const [showDropdownTheLoai, setShowDropdownTheLoai] = useState(false);
+    const [selectedValue, setSelectedValue] = useState('Danh sách');
+    const [isOn, setIsOn] = useState(false);
 
-    // Xử lý khi nhấn vào mục để hiển thị dropdown
-    const handleToggleDropdown = () => {
-        setShowDropdown(!showDropdown); // Đảo trạng thái giữa hiển thị và ẩn
+    useEffect(() => {
+        const savedMode = localStorage.getItem('darkMode');
+        if (savedMode === 'Dark') {
+            setIsOn(true);
+            document.body.classList.add("dark-mode");
+        }
+
+        const handleOutsideClick = (event) => {
+            if (
+                !event.target.closest('.header_row1_menu_dropdown_label') &&
+                !event.target.closest('.header_row1_menu_dropdown_menu')
+            ) {
+                setShowDropdownDanhSach(false);
+                setShowDropdownTheLoai(false);
+            }
+        };
+
+        document.addEventListener('click', handleOutsideClick);
+
+        return () => {
+            document.removeEventListener('click', handleOutsideClick);
+        };
+    }, []);
+
+    const handleToggle = () => {
+        setIsOn(!isOn);
+        if (!isOn) {
+            document.body.classList.add("dark-mode");
+            localStorage.setItem('darkMode', 'Dark');
+
+        } else {
+            document.body.classList.remove("dark-mode");
+            localStorage.setItem('darkMode', 'Light');
+
+        }
     };
 
-    // Xử lý khi chọn giá trị
+    const handleToggleDropdownDanhSach = () => {
+        setShowDropdownDanhSach(!showDropdownDanhSach);
+        if (showDropdownTheLoai) setShowDropdownTheLoai(false);
+    };
+
+    const handleToggleDropdownTheLoai = () => {
+        setShowDropdownTheLoai(!showDropdownTheLoai);
+        if (showDropdownDanhSach) setShowDropdownDanhSach(false);
+    };
+
     const handleSelect = (value) => {
-        setSelectedValue(value); 
-        setShowDropdown(false); 
+        setSelectedValue(value);
+        setShowDropdownDanhSach(false);
+        setShowDropdownTheLoai(false);
     };
 
     return (
-        <div className='header'>
-            <nav className='header_menu'>
-                <ul className='header-nav'>
-                    <li className='header-logo'>
-                        <h2>
-                            <a href="/">
-                                <i className="fa-solid fa-book-open"></i> Comics
-                            </a>
-                        </h2>
-                    </li>
-                    
-                    <li className='header_dropdown'>
-                        <span 
-                            className='dropdown_label' 
-                            onClick={handleToggleDropdown}
-                        >
-                            {selectedValue}
-                        </span>
-                        {showDropdown && (
-                            <ul className='dropdown_menu'>
-                                <li onClick={() => handleSelect('Truyện mới')}>Truyện mới</li>
-                                <li onClick={() => handleSelect('Truyện hot')}>Truyện hot</li>
-                            </ul>
-                        )}
-                    </li>
-                </ul>
-            </nav>
-            {/* Hiển thị thông điệp */}
-            <p>{selectedValue === 'Danh sách' ? 'Đọc truyện vui vẻ' : `Bạn đã chọn: ${selectedValue}`}</p>
+        <div>
+            <div className='header'>
+                <nav className='header_row1'>
+                    <ul className='header_row1_menu'>
+                        <li className='header_row1_menu_logo'>
+                            <h2>
+                                <a href="/">
+                                    <i className="fa-solid fa-book-open"></i> Comics
+                                </a>
+                            </h2>
+                        </li>
+
+                        {/* Dropdown "Danh sách" */}
+                        <li className='header_row1_menu_dropdown'>
+                            <div
+                                className='header_row1_menu_dropdown_label'
+                                onClick={handleToggleDropdownDanhSach}
+                            >
+                                <i className="fa-solid fa-list"></i>
+                                Danh sách
+                                <i className="fa-solid fa-caret-down"></i>
+                            </div>
+                            {showDropdownDanhSach && (
+                                <ul className='header_row1_menu_dropdown_menu show'>
+                                    <li
+                                        className='header_row1_menu_dropdown_menu_item'
+                                        onClick={() => handleSelect('Truyện mới')}
+                                    >
+                                        Truyện mới
+                                    </li>
+                                    <li
+                                        className='header_row1_menu_dropdown_menu_item'
+                                        onClick={() => handleSelect('Truyện hot')}
+                                    >
+                                        Truyện hot
+                                    </li>
+                                </ul>
+                            )}
+                        </li>
+
+                        <li className='header_row1_menu_dropdown'>
+                            <div
+                                className='header_row1_menu_dropdown_label'
+                                onClick={handleToggleDropdownTheLoai}
+                            >
+                                <i className="fa-solid fa-list"></i>
+                                Thể loại
+                                <i className="fa-solid fa-caret-down"></i>
+                            </div>
+                            {showDropdownTheLoai && (
+                                <ul className='header_row1_menu_dropdown_menu show'>
+                                    <li
+                                        className='header_row1_menu_dropdown_menu_item'
+                                        onClick={() => handleSelect('Hành động')}
+                                    >
+                                        Hành động
+                                    </li>
+                                    <li
+                                        className='header_row1_menu_dropdown_menu_item'
+                                        onClick={() => handleSelect('Kỳ ảo')}
+                                    >
+                                        Kỳ ảo
+                                    </li>
+                                    <li
+                                        className='header_row1_menu_dropdown_menu_item'
+                                        onClick={() => handleSelect('Tình cảm')}
+                                    >
+                                        Tình cảm
+                                    </li>
+                                    <li
+                                        className='header_row1_menu_dropdown_menu_item'
+                                        onClick={() => handleSelect('Phiêu lưu')}
+                                    >
+                                        Phiêu lưu
+                                    </li>
+                                    <li
+                                        className='header_row1_menu_dropdown_menu_item'
+                                        onClick={() => handleSelect('Kinh dị')}
+                                    >
+                                        Kinh dị
+                                    </li>
+                                </ul>
+                            )}
+                        </li>
+                        <li className="toggle">
+                            <div
+                                className={`toggle_btn ${isOn ? "Dark" : "Light"}`}
+                                onClick={handleToggle}
+                            >
+                                <div className="toggle_btn_thumb"></div>
+                            </div>
+                            <p>{isOn ? "Dark" : "Light"}</p>
+                        </li>
+                        <li>
+                            <div className='header_row1_menu_search'>
+                                <input type="text" id="search-input" className="header_row1_menu_search_input" placeholder="Tìm kiếm..." />
+                                <button className="header_row1_menu_search_btn">
+                                    <i className="fa fa-search"></i>
+                                </button>
+                            </div>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+            <div className='header_tb'>
+                <p>
+                    {selectedValue === 'Danh sách' ? 'Đọc truyện vui vẻ' : `${selectedValue}`}
+                </p>
+            </div>
         </div>
     );
 };
